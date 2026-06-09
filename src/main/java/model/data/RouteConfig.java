995d6426e5bf3591021c6f96ed1ffbc2639e5f7f
KevinMB0220 <kevin.brenes0220@gmail.com>
2026-06-08 23:07:19 -0600

    feat: add issues route mapping to RouteConfig

--- src/main/java/model/data/RouteConfig.java
+++ src/main/java/model/data/RouteConfig.java
@@ -8,9 +8,9 @@ public class RouteConfig {
     private final Map<String, GatewayRoute> routes = new HashMap<>();
 
     public RouteConfig() {
-        // Map clients and support routing keys to our Spring Boot backend service
         routes.put("clients", new GatewayRoute("clients", "http://localhost:8080/Backend/api/v1/clients"));
         routes.put("support", new GatewayRoute("support", "http://localhost:8080/Backend/api/v1/support"));
+        routes.put("issues", new GatewayRoute("issues", "http://localhost:8080/Backend/api/v1/issues"));
     }
 
     public GatewayRoute lookup(String key) {
